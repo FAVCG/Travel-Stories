a5829e5f973df3988ef925acbe0368d787f4a720
Faviola Caballero <fabycg18@gmail.com>
2020-03-02 23:33:27 -0800

    added thanks page

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -7,4 +7,21 @@ class SignupsController < ApplicationController
   def new
         @signup = Signup.new
     end
+
+     def create
+      @signup = Signup.new(signup_params)
+       if @signup.save
+         redirect_to '/thanks'
+       else
+         render 'new'
+      end
+
+    end
+
+  private
+   def signup_params
+    params.require(:firstname).permit(:email).permit
+   end
+
+
 end
