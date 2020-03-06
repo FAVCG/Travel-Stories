04f0e3731861c8b4d36b631ce60faccce36fe766
Faviola Caballero <fabycg18@gmail.com>
2020-03-05 21:59:11 -0800

    added create and param actions

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -5,6 +5,20 @@ class SignupsController < ApplicationController
   end
 
   def new
-        @signup = Signup.new
+    @signup = Signup.new
+  end
+
+  def create
+      @signup = Signup.new(signup_params)
+    if @signup.save
+      redirect_to '/thanks'
+    else
+      render 'new'
     end
+  end
+
+  private
+   def signup_params
+    params.require(:firstname).permit(:email)
+   end
 end
