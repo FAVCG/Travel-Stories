576b5ac9457364cc4f4199d0b69e5e2307ffc0f1
Faviola Caballero <fabycg18@gmail.com>
2020-03-05 21:24:16 -0800

    chnaged wording

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'about' => 'about#about'
   resources :signups
   post 'signups' => 'signups#create'
-
+  get 'thanks' => 'pages#thanks'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
