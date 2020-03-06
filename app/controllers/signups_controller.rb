06f07ef066be6d48c93b51dbf8f3e48a0dc8c282
Faviola Caballero <fabycg18@gmail.com>
2020-03-05 22:28:38 -0800

    action error

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -8,17 +8,17 @@ class SignupsController < ApplicationController
     @signup = Signup.new
   end
 
-  def create
-      @signup = Signup.new(signup_params)
-    if @signup.save
-      redirect_to '/thanks'
-    else
-      render 'new'
-    end
-  end
+  # def create
+  #     @signup = Signup.new(signup_params)
+  #   if @signup.save
+  #     redirect_to '/thanks'
+  #   else
+  #     render 'new'
+  #   end
+  # end
 
-  private
-   def signup_params
-    params.require(:firstname).permit(:email).permit
-   end
+  # private
+  #  def signup_params
+  #   params.require(:firstname).permit(:email).permit
+  #  end
 end
